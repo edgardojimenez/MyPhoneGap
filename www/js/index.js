3c9b5d4cbfaba53e80ac749a1039d1544e53868e
Edgardo Jimenez <edgardo.jimenez@live.com>
2014-05-29 23:10:17 -0500

    Adding scanner files

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,51 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener("deviceready", onDeviceReady, false);
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function onDeviceReady() {
+    navigator.splashscreen.hide();
+    var app = new App();
+    app.run();
+}
 
-        console.log('Received Event: ' + id);
+function App() {
+}
+
+App.prototype = {
+    resultsField: null,
+
+    run: function() {
+        var that = this,
+            scanButton = document.getElementById("scanButton");
+
+        that.resultsField = document.getElementById("result");
+
+        scanButton.addEventListener("click",
+            function() {
+                that._scan.call(that);
+            });
+    },
+
+    _scan: function() {
+        var that = this;
+        if (window.navigator.simulator === true) {
+            alert("Not Supported in Simulator.");
+        }
+        else {
+            cordova.plugins.barcodeScanner.scan(
+                function(result) {
+                    if (!result.cancelled) {
+                        that._addMessageToLog(result.format + " | " + result.text);
+                    }
+                },
+                function(error) {
+                    console.log("Scanning failed: " + error);
+                });
+        }
+    },
+
+    _addMessageToLog: function(message) {
+        var that = this,
+            currentMessage = that.resultsField.innerHTML;
+
+        that.resultsField.innerHTML = currentMessage + message + '<br />';
     }
-};
+}
